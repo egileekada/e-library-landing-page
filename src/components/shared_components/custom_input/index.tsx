51d87c396252c8071fea2dbdf86dd7ea3f6d96eb
egileekada <egileoniso.ekada@gmail.com>
2024-03-03 20:01:51 +0100

    adjustment on custom input component

--- src/components/shared_components/custom_input/index.tsx
+++ src/components/shared_components/custom_input/index.tsx
@@ -1,4 +1,4 @@
-import { InputGroup, InputLeftElement, Input, InputRightElement, Box, Text } from "@chakra-ui/react"
+import { InputGroup, InputLeftElement, Input, InputRightElement, Box, Text, Textarea } from "@chakra-ui/react"
 import React from "react"
 import { motion } from "framer-motion";
 
@@ -11,10 +11,11 @@ interface Props {
     h?: string,
     touch?: any,
     error?: any,
+    textarea?: boolean,
     [x: string]: any;
 }
 
-export default function InputComponent({ left, leftIcon, right, rightIcon, type, touch, error, h, ...rest }: Props) {
+export default function InputComponent({ left, leftIcon, right, rightIcon, type, touch, error, textarea, h, ...rest }: Props) {
 
     // const [showPassword, setShowPassword] = React.useState(false)
     const [intialType, setIntialType] = React.useState(type)
@@ -24,43 +25,48 @@ export default function InputComponent({ left, leftIcon, right, rightIcon, type,
             setIntialType("password")
         } else if (intialType === "password") {
             setIntialType("text")
-        }  else {
+        } else {
             setIntialType(type)
         }
     }
 
     return (
         <>
-            <InputGroup >
-                {left && (
-                    <InputLeftElement
-                        children={
-                            <Box display="flex" height={h ? h : "35px"} justifyContent="center" alignItems="center" marginTop="6px" marginLeft="12px" >
-                                {leftIcon}
-                            </Box>
-                        }
-                    />
-                )}
-                <Input {...rest} type={intialType} textColor="#000" paddingLeft={left ? "45px" : ""} fontSize="14px" fontWeight="400" bgColor="#FCFCFC" borderColor="#BDBDBD" _hover={{ borderColor: "#BDBDBD" }} _focus={{ backgroundColor: "#FCFCFC" }} focusBorderColor="#BDBDBD" height={h ? h : "45px"} />
-                {right && (
-                    <InputRightElement
-                        children={
-                            <Box display="flex" height={h ? h : "35px"} justifyContent="center" alignItems="center" marginTop="6px" paddingRight="30px" marginLeft="12px" >
-                                <Box type="button" as="button" fontSize={"14px"} fontWeight={"600"} onClick={() => ViewPassword()} >
-                                    {rightIcon && (
-                                        rightIcon
-                                    )}
-                                    {!rightIcon && (
-                                        <>
-                                            {intialType === "password" ? "Show" : "Hide"}
-                                        </>
-                                    )}
+            {!textarea && ( 
+                <InputGroup >
+                    {left && (
+                        <InputLeftElement
+                            children={
+                                <Box display="flex" height={h ? h : "35px"} justifyContent="center" alignItems="center" marginTop="6px" marginLeft="12px" >
+                                    {leftIcon}
                                 </Box>
-                            </Box>
-                        }
-                    />
-                )}
-            </InputGroup>
+                            }
+                        />
+                    )}
+                    <Input {...rest} type={intialType} textColor="#000" paddingLeft={left ? "45px" : ""} fontSize="14px" fontWeight="400" bgColor="#FCFCFC" borderColor="#BDBDBD" _hover={{ borderColor: "#BDBDBD" }} _focus={{ backgroundColor: "#FCFCFC" }} focusBorderColor="#BDBDBD" height={h ? h : "45px"} />
+                    {right && (
+                        <InputRightElement
+                            children={
+                                <Box display="flex" height={h ? h : "35px"} justifyContent="center" alignItems="center" marginTop="6px" paddingRight="30px" marginLeft="12px" >
+                                    <Box type="button" as="button" fontSize={"14px"} fontWeight={"600"} onClick={() => ViewPassword()} >
+                                        {rightIcon && (
+                                            rightIcon
+                                        )}
+                                        {!rightIcon && (
+                                            <>
+                                                {intialType === "password" ? "Show" : "Hide"}
+                                            </>
+                                        )}
+                                    </Box>
+                                </Box>
+                            }
+                        />
+                    )}
+                </InputGroup>
+            )}
+            {textarea && (
+                <Textarea  {...rest} textColor="#000" paddingLeft={left ? "45px" : ""} fontSize="14px" fontWeight="400" bgColor="#FCFCFC" borderColor="#BDBDBD" _hover={{ borderColor: "#BDBDBD" }} _focus={{ backgroundColor: "#FCFCFC" }} focusBorderColor="#BDBDBD" height={h ? h : "auto"} />
+            )}
             {touch && error && (
                 <Text as={motion.p}
                     initial={{ y: -100, opacity: 0 }}
