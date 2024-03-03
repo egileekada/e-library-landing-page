83b0d87c174fc0609576ad888f0a70ad875234b5
egileekada <egileoniso.ekada@gmail.com>
2024-03-03 20:02:49 +0100

    built partner card component for each partner

--- src/components/elibrary_components/tiles.tsx
+++ src/components/elibrary_components/tiles.tsx
@@ -1,11 +1,16 @@
-import { Box, Button, Flex, Text } from '@chakra-ui/react'
+import { Box, Button, Flex, Image, Text } from '@chakra-ui/react'
 import { PinIcon } from '../shared_components/svg'
-// import React from 'react'
+import { IPartner } from '../../models'
+// import React from 'react' 
 
-interface Props {}
-
-function Tiles(props: Props) {
-    const {} = props
+function Tiles(props: IPartner) {
+    const {
+        imageUrl,
+        partnerName,
+        partnerResourceName,
+        partnerResourceUrl,
+        id
+    } = props
 
     return (
         <Box pos={"relative"} p={"4"} >
@@ -13,18 +18,21 @@ function Tiles(props: Props) {
                 <PinIcon />
             </Box>
             <Flex gap={"2"} alignItems={"end"} >
-                <Box w={"80px"} h={"80px"} bgColor={"green"} />
-                <Flex bgColor={"#DEFFEB"} rounded={"20px"} px={"12px"} py={"4px"} justifyContent={"center"} alignItems={"center"} >
+                <Box w={"80px"} h={"80px"} bgColor="gray" >
+                    <Image w={"full"} h={"full"} objectFit={"cover"} alt='parnter' src={imageUrl} />
+                </Box>
+                {/* <Flex bgColor={"#DEFFEB"} rounded={"20px"} px={"12px"} py={"4px"} justifyContent={"center"} alignItems={"center"} >
                     <Text fontSize={"14px"} lineHeight={"20.3px"} color={"#00451C"} >Available</Text>
-                </Flex>
+                </Flex> */}
             </Flex>
-            <Text color={"#1E1B39"} lineHeight={"21.7px"} fontSize={"18px"} fontWeight={"600"} mt={"4"} >Microsoft Viva Learning</Text>
-            <Text fontSize={"14px"} lineHeight={"23.2px"} color={"#828282"} >Partner: Microsoft</Text>
-            <Text fontSize={"14px"} lineHeight={"23.2px"} color={"#828282"} >PID Number: 125678</Text>
-
-            <Button h={"45px"} gap={"2"} rounded={"5px"} width={"full"} mt={"4"} bgColor={"#1F7CFF"} _hover={{backgroundColor: "#1F7CFF"}} display={"flex"} alignItems={"center"} justifyContent={"center"} color={"white"} >
-                View
-            </Button>
+            <Text color={"#1E1B39"} lineHeight={"21.7px"} fontSize={"18px"} fontWeight={"600"} mt={"4"} >{partnerName}</Text>
+            <Text fontSize={"14px"} lineHeight={"23.2px"} color={"#828282"} >Partner: {partnerResourceName}</Text>
+            <Text fontSize={"14px"} lineHeight={"23.2px"} color={"#828282"} >PID Number: {id}</Text>
+            <a href={partnerResourceUrl} target={"_blank"} > 
+                <Button h={"45px"} gap={"2"} rounded={"5px"} width={"full"} mt={"4"} bgColor={"#1F7CFF"} _hover={{ backgroundColor: "#1F7CFF" }} display={"flex"} alignItems={"center"} justifyContent={"center"} color={"white"} >
+                    View
+                </Button>
+            </a>
         </Box>
     )
 }
